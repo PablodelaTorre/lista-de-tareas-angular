7ee4192daa74194720847368e6b55ba4a8401d1c
Pablo <delatorre.pablodaniel@gmail.com>
2022-09-07 15:53:37 -0300

    mock agregado

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,16 @@
+import {Task} from './Task'
+
+export const TASK : Task[] = [
+    {
+        id:1,
+        text:"Terminar primer modulo",
+        day:'4 de agosto',
+        reminder: true
+    },
+    {
+        id:2,
+        text:"Terminar segundo modulo",
+        day:'4 de septiembre',
+        reminder: false
+    }
+]
